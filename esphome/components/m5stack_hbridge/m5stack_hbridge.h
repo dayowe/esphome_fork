c714b366b5e4b86910c6072b2605d89ad922f42a
Dayowe <dayowe@tutamail.com>
2024-06-13 18:41:19 +0200

    Test

--- esphome/components/m5stack_hbridge/m5stack_hbridge.h
+++ esphome/components/m5stack_hbridge/m5stack_hbridge.h
@@ -1,56 +1,33 @@
-#ifndef __M5STACK_HBRIDGE_H
-#define __M5STACK_HBRIDGE_H
+#pragma once
 
 #include "esphome/core/component.h"
 #include "esphome/components/i2c/i2c.h"
-#include "M5UnitHbridge.h"  // Include the M5UnitHbridge library
 
 namespace esphome {
 namespace m5stack_hbridge {
 
+enum hbridge_direction_t {
+  HBRIDGE_FORWARD,
+  HBRIDGE_BACKWARD,
+  HBRIDGE_STOP
+};
+
 class M5StackHBridge : public Component, public i2c::I2CDevice {
  public:
-  void setup() override {
-    // Initialize the H-Bridge
-    if (!driver.begin(&Wire, address_, sda_, scl_, speed_)) {
-      ESP_LOGE("m5stack_hbridge", "H-Bridge not found");
-    }
-  }
-
-  void loop() override {
-    // Loop function for periodic tasks
-  }
-
-  void set_direction(hbridge_direction_t direction) {
-    driver.setDriverDirection(direction);
-  }
-
-  void set_speed(uint8_t speed) {
-    driver.setDriverSpeed8Bits(speed);
-  }
-
-  void set_i2c_pins(uint8_t sda, uint8_t scl) {
-    sda_ = sda;
-    scl_ = scl;
-  }
-
-  void set_i2c_address(uint8_t address) {
-    address_ = address;
-  }
-
-  void set_i2c_speed(uint32_t speed) {
-    speed_ = speed;
-  }
+  void setup() override;
+  void loop() override;
+  void set_direction(hbridge_direction_t direction);
+  void set_speed(uint8_t speed);
+  void set_i2c_pins(uint8_t sda, uint8_t scl);
+  void set_i2c_address(uint8_t address);
+  void set_i2c_speed(uint32_t speed);
 
  private:
-  M5UnitHbridge driver;
   uint8_t sda_;
   uint8_t scl_;
-  uint8_t address_ = HBRIDGE_I2C_ADDR;
-  uint32_t speed_ = 100000L;
+  uint8_t i2c_address_;
+  uint32_t i2c_speed_;
 };
 
 }  // namespace m5stack_hbridge
 }  // namespace esphome
-
-#endif  // __M5STACK_HBRIDGE_H
